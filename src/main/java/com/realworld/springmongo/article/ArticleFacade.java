5bc72584d38829a3f72d46483c1355831136515c
iryabov <to.iryabov@gmail.com>
2024-06-07 13:34:08 +0200

    feat: add println in createArticle

--- src/main/java/com/realworld/springmongo/article/ArticleFacade.java
+++ src/main/java/com/realworld/springmongo/article/ArticleFacade.java
@@ -32,6 +32,7 @@ public class ArticleFacade {
     }
 
     public Mono<ArticleView> createArticle(CreateArticleRequest request, User author) {
+        System.out.println("test-2");
         var id = UUID.randomUUID().toString();
         var newArticle = request.toArticle(id, author.getId());
         return articleRepository.save(newArticle)
